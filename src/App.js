a067f7639aad97f6fe343d42240cde302a1b9a66
PCJ-93 <pw2900@naver.com>
2025-03-27 12:39:51 +0900

    포폴 비디오 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="portContainer">
+
+      <video autoPlay loop muted className="background-video1">
+        <source src="/videos/Prism.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <video autoPlay loop muted className="background-video2">
+        <source src="/videos/TravelMaker.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <video autoPlay loop muted className="background-video3">
+        <source src="/videos/GreenConnect.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
     </div>
   );
 }
