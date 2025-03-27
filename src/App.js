79283b55bf1f8473c8150b1f30456067d1efe2af
PCJ-93 <pw2900@naver.com>
2025-03-27 12:49:17 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
     <div className="portContainer">
 
       <video autoPlay loop muted className="background-video1">
-        <source src="/videos/Prism.mp4" type="video/mp4" />
+        <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/Prism.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
       <video autoPlay loop muted className="background-video2">
-        <source src="/videos/TravelMaker.mp4" type="video/mp4" />
+        <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/TravelMaker.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
       <video autoPlay loop muted className="background-video3">
-        <source src="/videos/GreenConnect.mp4" type="video/mp4" />
+        <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/GreenConnect.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
