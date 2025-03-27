fde88da64f5dfa52b37ff2d33f127961745e816a
PCJ-93 <pw2900@naver.com>
2025-03-27 13:58:12 +0900

    3차 수정

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ function App() {
   return (
     <div className="portContainer">
 
-      <div>
+      <div className="video-container">
         <video autoPlay loop muted className="background-video1">
           <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/Prism.mp4" type="video/mp4" />
         </video>
       </div>
 
-      <div>
+      <div className="video-container">
         <video autoPlay loop muted className="background-video2">
           <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/TravelMaker.mp4" type="video/mp4" />
         </video>
       </div>
-      <div>
+      
+      <div className="video-container">
         <video autoPlay loop muted className="background-video3">
           <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/GreenConnect.mp4" type="video/mp4" />
         </video>
