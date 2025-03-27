2d95e3b5a451d3d4bbcc5f7137c294a421e638a3
PCJ-93 <pw2900@naver.com>
2025-03-27 13:54:07 +0900

    2차 수정

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ function App() {
   return (
     <div className="portContainer">
 
-      <video autoPlay loop muted className="background-video1">
-        <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/Prism.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <div>
+        <video autoPlay loop muted className="background-video1">
+          <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/Prism.mp4" type="video/mp4" />
+        </video>
+      </div>
 
-      <video autoPlay loop muted className="background-video2">
-        <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/TravelMaker.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-
-      <video autoPlay loop muted className="background-video3">
-        <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/GreenConnect.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <div>
+        <video autoPlay loop muted className="background-video2">
+          <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/TravelMaker.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div>
+        <video autoPlay loop muted className="background-video3">
+          <source src="https://PCJ-93.github.io/pcj-web-portfolio/videos/GreenConnect.mp4" type="video/mp4" />
+        </video>
+      </div>
 
     </div>
   );
